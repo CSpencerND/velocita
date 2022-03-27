03a3170dbfadd0a00a008c629c4b24d2d3d4dec1
cs <cspencernd@gmail.com>
2022-03-27 15:31:56 -0400

    modal working

---
+++ script/gallery.js
@@ -0,0 +1,28 @@
+const showcase = document.getElementById('showcase')
+const imgs = showcase.querySelectorAll('img')
+
+const modal = document.getElementById('img-modal')
+const modalContent = modal.querySelector('img')
+
+const body = document.querySelector('body')
+
+imgs.forEach(
+    (img) =>
+        (img.onclick = (event) => {
+            modalContent.src = event.target.src
+            modal.classList.add('modal--active')
+            body.classList.add('modal--active')
+        })
+)
+
+modal.onclick = () => {
+    modal.classList.remove('modal--active')
+    body.classList.remove('modal--active')
+}
+
+document.onkeydown = (event) => {
+    if (event.key === 'Escape') {
+        modal.classList.remove('modal--active')
+        body.classList.remove('modal--active')
+    }
+}
