b5b7ceff9a7699f32d60ba22c0b427ae5c295f97
cs <cspencernd@gmail.com>
2022-03-29 15:03:07 -0400

    bottom nav integrate on non-mobile

--- script/main.js
+++ script/main.js
@@ -7,21 +7,12 @@ toggle.onclick = () => {
 }
 
 const bottomNav = document.getElementById('bottom-navigation')
-// const scrollArrow = document.getElementById('scroll-arrow')
 let lastScrollY = window.scrollY
 window.onscroll = () => {
     if (lastScrollY < window.scrollY) {
         bottomNav.classList.add('nav-hidden')
-        // scrollArrow.innerHTML =
-        //     '<i class="fa-solid fa-arrow-up"></i>'
-        // scrollArrow.href = '#'
-        // scrollArrow.style = 'display: none'
     } else {
         bottomNav.classList.remove('nav-hidden')
-        // scrollArrow.innerHTML =
-        //     '<i class="fa-solid fa-arrow-down"></i>'
-        // scrollArrow.style = 'display: initial; opacity: .67'
-        // scrollArrow.href = '#about'
     }
     lastScrollY = window.scrollY
 }
